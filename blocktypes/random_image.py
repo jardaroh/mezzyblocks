ad52221c79902b96460e534dfe2c979ffbc78f52
Jardar <jardar@Jardarbuntu.(none)>
2013-01-24 14:15:37 +0100

    Implemented a Patch given through Gist by Ryochan7 to fix errors occuring in the random block blocktype when the gallery specified did not exist in database

--- blocktypes/random_image.py
+++ blocktypes/random_image.py
@@ -2,6 +2,7 @@ from random import choice
 
 from django.template import Context, Template
 from django.template.loader import get_template, render_to_string
+from django.core.exceptions import ObjectDoesNotExist
 
 from mezzanine.conf import settings
 from mezzanine.galleries.models import Gallery
@@ -21,14 +22,27 @@ block_settings = (
 
 def getSetting( block, setting, default=None ):
 	"""Get a settings value configured for a block, or it's default."""
-	value = BlockConfig.objects.get(
-		block_id = block.id,
-		setting = BlockTypeSetting.objects.get(
-			blocktype = block.blocktype_id,
-			setting_name = setting )
-	).value
+	# value = BlockConfig.objects.get(
+	# 	block_id = block.id,
+	# 	setting = BlockTypeSetting.objects.get(
+	# 		blocktype = block.blocktype_id,
+	# 		setting_name = setting )
+	# ).value
 
-	if not value: value = default
+	# if not value: value = default
+
+	value = default
+	try:
+		value = BlockConfig.objects.get(
+			block_id = block.id,
+			setting = BlockTypeSetting.objects.get(
+				blocktype = block.blocktype_id,
+				setting_name = setting
+			)
+		).value
+	except ObjectDoesNotExist:
+		pass
+
 	return value
 
 
@@ -36,13 +50,29 @@ def block_context_processor(block):
 
 	gallery = getSetting( block, "gallery", block_settings[0][2] )
 
-	folder = Gallery.objects.get(title=gallery)
-	files = folder.images.all()
-	rand_file = choice(files)
+	# folder = Gallery.objects.get(title=gallery)
+	# files = folder.images.all()
+	# rand_file = choice(files)
 
-	title = rand_file.description
-	image = settings.MEDIA_URL+str(rand_file.file)
-	image = image[1:]
+	try:
+		folder = Gallery.objects.get(title=gallery)
+	except Gallery.DoesNotExist:
+		folder = None
+
+	title = ""
+	image = None
+	if folder:
+		files = folder.images.all()
+		rand_file = choice(files)
+
+	# title = rand_file.description
+	# image = settings.MEDIA_URL+str(rand_file.file)
+	# image = image[1:]
+
+		if rand_file:
+			title = rand_file.description
+			image = settings.MEDIA_URL+str(rand_file.file)
+			image = image[1:]
 
 	#Fill this context with any KEY, VALUE pairs
 	#to pass them to the template
